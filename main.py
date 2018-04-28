6610087aeb0bdc0da3024f9a59da2f704dad3abb
lvang0123 <37590716+lvang0123@users.noreply.github.com>
2018-04-28 00:10:36 -0400

    Update main.py
    Fixed the error that Instructor Kathy Ison brought up.

--- main.py
+++ main.py
@@ -11,13 +11,25 @@ testScore = 0.0
 testAverage = 0.0
 
 #introduction, welcoming the user
-print ("Welcome to the Average Test Score program!")
+print("Welcome to the Average Test Score program!")
 
 #creates an empty line
 print()
 
 #user's input of the total number of tests 
-numberOfTests = input("How many tests have you taken?")
+while True:
+  try:
+    numberOfTests = int(input('How many tests have you taken?'))
+    if (numberOfTests <= 0):
+      print()
+      print("*** ERROR: Must enter at least 1 ***")
+      print()
+    else:
+      break
+  except ValueError:
+    print()
+    print("*** ERROR: Not a number format ***")
+    print()
 
 #creates an empty line
 print()
@@ -25,7 +37,25 @@ print()
 #for loop, inputting the user's test scores and adding them together
 for x in range(0, numberOfTests):
     print(("Test Number %d") % (x + 1))
-    testScore = input("Enter your test score:")
+    
+    while True:
+      try:
+        testScore = int(input("Enter your test score:"))
+        if (testScore < 0):
+          print()
+          print("*** ERROR: Test score cannot be lower than 0 ***")
+          print()
+        elif (testScore > 100):
+          print()
+          print("*** ERROR: Test score cannot be higher than 100 ***")
+          print()
+        else:
+          break
+      except ValueError:
+        print()
+        print("*** ERROR: Not a number format ***")
+        print()
+    
     print()
     totalTests = totalTests + testScore
 
@@ -39,9 +69,7 @@ print(("Average Test Score:"), (format(testAverage, '.2f')), ("%"))
 print()
 
 #if...elif...else statements to find and output the correct letter grade for the average test score 
-if (testAverage > 100):
-  print("You've received an A+ for your letter grade!")
-elif (testAverage >= 90):
+if (testAverage >= 90):
   print("You've received an A for your letter grade!")
 elif (testAverage >= 80 and testAverage <=89):
   print("You've received a B for your letter grade!")
