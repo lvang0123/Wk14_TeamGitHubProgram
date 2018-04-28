515d07d66eb6add6c0f250a592f6edd23121d299
lvang0123 <37590716+lvang0123@users.noreply.github.com>
2018-04-27 23:35:00 -0400

    Update main.py
    Fixed all the "print" function.

--- main.py
+++ main.py
@@ -11,49 +11,49 @@ testScore = 0.0
 testAverage = 0.0
 
 #introduction, welcoming the user
-print "Welcome to the Average Test Score program!"
+print ("Welcome to the Average Test Score program!")
 
 #creates an empty line
-print
+print()
 
 #user's input of the total number of tests 
 numberOfTests = input("How many tests have you taken?")
 
 #creates an empty line
-print
+print()
 
 #for loop, inputting the user's test scores and adding them together
 for x in range(0, numberOfTests):
-    print "Test Number %d" % (x + 1)
+    print(("Test Number %d") % (x + 1))
     testScore = input("Enter your test score:")
-    print
+    print()
     totalTests = totalTests + testScore
 
 #calculating average
 testAverage = (totalTests / numberOfTests)
 
 #outputs & formats the average to have 2 decimal places
-print "Average Test Score:", format(testAverage, '.2f'), "%"
+print(("Average Test Score:"), (format(testAverage, '.2f')), ("%"))
 
 #creates an empty line
-print
+print()
 
 #if...elif...else statements to find and output the correct letter grade for the average test score 
 if (testAverage > 100):
-  print "You've received an A+ for your letter grade!"
+  print("You've received an A+ for your letter grade!")
 elif (testAverage >= 90):
-  print "You've received an A for your letter grade!"
+  print("You've received an A for your letter grade!")
 elif (testAverage >= 80 and testAverage <=89):
-  print "You've received a B for your letter grade!"
+  print("You've received a B for your letter grade!")
 elif (testAverage >= 70 and testAverage <=79):
-  print "You've received a C for your letter grade!"
+  print("You've received a C for your letter grade!")
 elif (testAverage >= 60 and testAverage <=69):
-  print "You've received a D for your letter grade!"
+  print("You've received a D for your letter grade!")
 else: 
-  print "You've received a F for your letter grade!"
+  print("You've received a F for your letter grade!")
 
 #creates an empty line  
-print
+print()
 
 #informing the user that the program has ended
 print "Thank you for using the Average Test Score program!"
